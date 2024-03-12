d0e446999ce46f8465d30e2dccd03a5437c8d654
ferchichi ghofrane <ferchichighofrane2000@gmail.com>
2024-03-12 11:10:04 +0100

    creation user

--- api-app/src/users/dto/create-user.dto.ts
+++ api-app/src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsEmail } from "class-validator"
+
+export class CreateUserDto {
+@IsEmail()
+@ApiProperty()
+    firstName :string ;
+    @ApiProperty()
+    lastName :string ;
+    @ApiProperty()
+    email :string ;
+    @ApiProperty()
+    password :string;
+    @ApiProperty()
+    phone :string;
+    @ApiProperty()
+    address :string;
+    @ApiProperty()
+    occupation :string;
+}
--- api-app/src/users/users.service.ts
+++ api-app/src/users/users.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {
+  }
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    // This action adds a new user
+    return this.prisma.user.create({
+      data:dto
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    // This action returns all users
+    return this.prisma.user.findMany({
+
+    });
   }
 
   findOne(id: number) {
